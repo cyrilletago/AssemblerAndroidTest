525477190582944db89fdba7f436e337c273b011
cyrillelenou <cyrille.lenou.001@student.uni.lu>
2018-04-13 16:35:49 +0200

    non-Functional version: new modif

--- jni/main.c
+++ jni/main.c
@@ -1,41 +1,32 @@
+#include <inttypes.h>
 #include <stdio.h>
+#include <string.h>
 
-#include <jni.h>
-/* This stub calls the function. */
+int int_add_128(uint32_t *r, const uint32_t *a, const uint32_t *b);
 
-#ifdef __ARM__
-int asm_main(void);
-int add_asm(int a, int b);
-#endif
-
-jstring Java_com_cyrille_assemblytest_MainActivity_jniMethod(
-        JNIEnv* env, jobject thiz) {
-    enum Constexpr { N = 256 };
-    char s[N];
-    size_t cur = 0;
-
-    int x = 0;
-
-#ifdef __ARM__
-    /* This part of the code seems not to be executed as the value of x in the if loop
-         * remains 0 even after the redefinition below.*/
-
-    // cur += snprintf(s + cur, N - cur, "arm ");
-
-    x = 1;
-
-    x += asm_main();
-    x += add_asm(1000, 4000);
-#endif
-
-
-//    if (x == 1)
-//        cur += snprintf(s + cur, N - cur, " Hello: x is %s", "1");
-//    else
-//        cur += snprintf(s + cur, N - cur, " Hello: x is %s", "-");
-
-    cur += snprintf(s + cur, N - cur, " Hello %d", x);
-    /*cur +=a snprintf(s + cur, N - cur, "Hello %d", asm_main());*/
-
-    return (*env)->NewStringUTF(env, s);
+void int_print(const char *c, uint32_t *a, int len)
+{
+  int i;
+  
+  if ((c != NULL) && (strlen(c) > 0)) printf("%s", c);
+  for (i = len - 1; i >= 0; i --) printf("%08x", a[i]);
+  printf("\n");
 }
+
+int main(void)
+{
+  int c = 5;
+  unsigned int a[4] = { 10, 20, 30, 40 };
+  unsigned int b[4] = { 11, 22, 33, 44 };
+  unsigned int r[4];
+  
+  c = int_add_128(r, a, b);
+  
+  int_print("a = 0x", a, 4);
+  int_print("a = 0x", b, 4);
+  int_print("a = 0x", r, 4);
+  printf("c = %i\n", c);
+  
+  return 0;
+}
+
