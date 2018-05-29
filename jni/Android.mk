441cf18725b67e9a5482d4b22cdea59f88060af7
cyrillelenou <cyrille.lenou.001@student.uni.lu>
2018-05-29 15:26:50 +0200

    Build Functional, functionalities not tested

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,10 +4,10 @@ LOCAL_MODULE := main
 LOCAL_SRC_FILES := main.c
 # http://stackoverflow.com/questions/12614417/android-ndk-how-to-get-compiler-architecture-in-android-mk-dynamically
 
-ifeq ($(TARGET_ARCH_ABI),armeabi)
-	LOCAL_SRC_FILES := $(LOCAL_SRC_FILES) add_asm.S gfpmul.S
-    LOCAL_CFLAGS += -D__ARM__
-endif
+# ifeq ($(TARGET_ARCH_ABI),armeabi)
+# 	 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES) add_asm.S gfpmul.S
+#    LOCAL_CFLAGS += -D__ARM__
+# endif
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 	LOCAL_SRC_FILES := $(LOCAL_SRC_FILES) add_asm.S gfpmul.S
